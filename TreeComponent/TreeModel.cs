eec1fe1a85ec4c63e6714e9bc3aca74493126372
Fadki <zloizvyk@gmail.com>
2017-12-10 21:48:11 +0400

    создал в проекте дерева модель РУТ

---
+++ TreeComponent/TreeModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChartComponent;
+
+namespace TreeComponent
+{
+    class TreeModel
+    {
+        private List<ChartModel> children;
+        private string name = "root";
+
+
+        public string Name
+        {
+            get { return name; }
+        } 
+
+        public List<ChartModel> Children
+        {
+            set { children = value; }
+            get { return children; }
+        }
+
+        public TreeModel()
+        {
+            children = new List<ChartModel>();
+        }
+    }
+}
