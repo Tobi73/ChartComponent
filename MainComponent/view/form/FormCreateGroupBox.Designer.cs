4cc4e8a578763e3314f575d81bed893f8101bcb4
Fadki <zloizvyk@gmail.com>
2017-12-12 21:09:55 +0400

    изменил групбокс на панель

--- MainComponent/view/form/FormCreateGroupBox.Designer.cs
+++ MainComponent/view/form/FormCreateGroupBox.Designer.cs
@@ -28,92 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnLoad = new System.Windows.Forms.Button();
             this.btnConvert = new System.Windows.Forms.Button();
             this.btnTable = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.chartTree1 = new TreeComponent.ChartTree(this.components);
+            this.govnoblyapotom1 = new ChartComponent.Govnoblyapotom(this.components);
+            this.tableLayoutPanelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.govnoblyapotom1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.btnSave);
-            this.groupBox1.Controls.Add(this.btnLoad);
-            this.groupBox1.Controls.Add(this.btnConvert);
-            this.groupBox1.Controls.Add(this.btnTable);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(481, 247);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(87, 218);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 3;
-            this.btnSave.Text = "save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // btnLoad
-            // 
-            this.btnLoad.Location = new System.Drawing.Point(6, 218);
-            this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(75, 23);
-            this.btnLoad.TabIndex = 2;
-            this.btnLoad.Text = "load";
-            this.btnLoad.UseVisualStyleBackColor = true;
-            // 
-            // btnConvert
-            // 
-            this.btnConvert.Location = new System.Drawing.Point(319, 218);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(75, 23);
-            this.btnConvert.TabIndex = 1;
-            this.btnConvert.Text = "convert";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            // 
-            // btnTable
-            // 
-            this.btnTable.Location = new System.Drawing.Point(400, 218);
-            this.btnTable.Name = "btnTable";
-            this.btnTable.Size = new System.Drawing.Size(75, 23);
-            this.btnTable.TabIndex = 0;
-            this.btnTable.Text = "table";
-            this.btnTable.UseVisualStyleBackColor = true;
-            this.btnTable.Click += new System.EventHandler(this.btnTable_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSave.Location = new System.Drawing.Point(24, 145);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLoad.Location = new System.Drawing.Point(148, 145);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnConvert.Location = new System.Drawing.Point(272, 145);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            // 
+            // btnTable
+            // 
+            this.btnTable.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnTable.Location = new System.Drawing.Point(398, 145);
+            this.btnTable.Name = "btnTable";
+            this.btnTable.Size = new System.Drawing.Size(75, 23);
+            this.btnTable.TabIndex = 4;
+            this.btnTable.Text = "table";
+            this.btnTable.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 4;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.Controls.Add(this.btnSave, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.btnConvert, 2, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.btnLoad, 1, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.btnTable, 3, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.chartTree1, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.govnoblyapotom1, 2, 0);
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(66, 60);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80.4878F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.5122F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(499, 174);
+            this.tableLayoutPanelMain.TabIndex = 8;
+            // 
+            // chartTree1
+            // 
+            this.tableLayoutPanelMain.SetColumnSpan(this.chartTree1, 2);
+            this.chartTree1.Location = new System.Drawing.Point(3, 3);
+            this.chartTree1.Name = "chartTree1";
+            this.chartTree1.RootName = "Chart Tree";
+            this.chartTree1.Size = new System.Drawing.Size(242, 134);
+            this.chartTree1.TabIndex = 8;
+            this.chartTree1.Text = "chartTree1";
+            // 
+            // govnoblyapotom1
+            // 
+            this.govnoblyapotom1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.govnoblyapotom1.ChartAreas.Add(chartArea1);
+            this.tableLayoutPanelMain.SetColumnSpan(this.govnoblyapotom1, 2);
+            legend1.Name = "Legend1";
+            this.govnoblyapotom1.Legends.Add(legend1);
+            this.govnoblyapotom1.Location = new System.Drawing.Point(251, 3);
+            this.govnoblyapotom1.Name = "govnoblyapotom1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.govnoblyapotom1.Series.Add(series1);
+            this.govnoblyapotom1.Size = new System.Drawing.Size(245, 134);
+            this.govnoblyapotom1.TabIndex = 9;
+            this.govnoblyapotom1.Text = "govnoblyapotom1";
+            // 
             // FormCreateGroupBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(505, 271);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(736, 271);
+            this.Controls.Add(this.tableLayoutPanelMain);
             this.Name = "FormCreateGroupBox";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormCreateGroupBox_Load);
-            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.govnoblyapotom1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnLoad;
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Button btnTable;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private TreeComponent.ChartTree chartTree1;
+        private ChartComponent.Govnoblyapotom govnoblyapotom1;
     }
 }
