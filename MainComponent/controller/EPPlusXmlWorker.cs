88cc2274cae8b47ea77429825b074c5603af170e
Fadki <zloizvyk@gmail.com>
2017-12-19 16:04:35 +0400

    NUGET обовить, добавтил библиотеку EPPlus

---
+++ MainComponent/controller/EPPlusXmlWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MainComponent.controller
+{
+    class EPPlusXmlWorker
+    {
+
+        public void met()
+        {
+            //var query1 = (from cell in sheet.Cells["d:d"] where cell.Value is double && (double)cell.Value >= 9990 && (double)cell.Value <= 10000 select cell);
+        }
+    }
+}
